3aa19623fddf775ec6fbff27babba1e41e2fc531
Henrik Finsberg <henriknf@simula.no>
2026-07-05 13:44:18 +0200

    Add transferoperator for easier transfer between meshes and implement a test for this

--- src/simcardemsx/interpolation.py
+++ src/simcardemsx/interpolation.py
@@ -8,6 +8,34 @@ import ufl
 logger = logging.getLogger(__name__)
 
 
+class TransferOperator:
+    """
+    A generic operator to interpolate functions between two non-matching FEniCSx function spaces.
+    """
+
+    def __init__(self, V_source: dolfinx.fem.FunctionSpace, V_target: dolfinx.fem.FunctionSpace):
+        self.V_source = V_source
+        self.V_target = V_target
+
+        # In FEniCSx, you compute interpolation data on the *target* mesh cells
+        target_mesh = self.V_target.mesh
+        cell_map = target_mesh.topology.index_map(target_mesh.topology.dim)
+        num_cells = cell_map.size_local + cell_map.num_ghosts
+        self.cells_target = np.arange(num_cells, dtype=np.int32)
+
+        # Pre-compute non-matching interpolation data
+        self.interpolation_data = dolfinx.fem.create_interpolation_data(
+            self.V_target,
+            self.V_source,
+            self.cells_target,
+        )
+
+    def interpolate(self, u_source: dolfinx.fem.Function, u_target: dolfinx.fem.Function) -> None:
+        """Interpolates the source function into the target function in-place."""
+        u_target.interpolate_nonmatching(u_source, self.cells_target, self.interpolation_data)
+        u_target.x.scatter_forward()
+
+
 @dataclass
 class MissingValue:
     element: ufl.finiteelement.AbstractFiniteElement
@@ -19,6 +47,7 @@ class MissingValue:
     def __post_init__(self):
         self.V_ep = dolfinx.fem.functionspace(self.ep_mesh, self.element)
         self.V_mechanics = dolfinx.fem.functionspace(self.mechanics_mesh, self.element)
+
         self.V_ep_int = dolfinx.fem.functionspace(self.ep_mesh, self.interpolation_element)
         self.V_mechanics_int = dolfinx.fem.functionspace(
             self.mechanics_mesh,
@@ -36,33 +65,17 @@ class MissingValue:
         self.values_ep = np.zeros((self.num_values, self.u_ep[0].x.array.size))
         self.values_mechanics = np.zeros((self.num_values, self.u_mechanics[0].x.array.size))
 
-        # Mechanics to EP
-        cell_map = self.domain_ep.topology.index_map(self.domain_ep.topology.dim)
-        num_cells = cell_map.size_local + cell_map.num_ghosts
-        self.cells_mech2ep = np.arange(num_cells, dtype=np.int32)
-        self.V_mechanics_interpolation_data_mech2ep = dolfinx.fem.create_interpolation_data(
-            self.V_ep,
-            self.V_mechanics,
-            self.cells_mech2ep,
-        )
-
-        # EP to Mechanics
-        cell_map = self.domain_mechanics.topology.index_map(self.domain_mechanics.topology.dim)
-        num_cells = cell_map.size_local + cell_map.num_ghosts
-        self.cells_ep2mech = np.arange(num_cells, dtype=np.int32)
-        self.V_ep_interpolation_data_ep2mech = dolfinx.fem.create_interpolation_data(
-            self.V_mechanics,
-            self.V_ep,
-            self.cells_ep2mech,
-        )
+        # Setup Transfer Operators instead of manual interpolation data
+        self.transfer_ep2mech = TransferOperator(V_source=self.V_ep_int, V_target=self.V_mechanics)
+        self.transfer_mech2ep = TransferOperator(V_source=self.V_mechanics_int, V_target=self.V_ep)
 
     @property
     def domain_mechanics(self):
-        return self.V_mechanics.mesh
+        return self.mechanics_mesh
 
     @property
     def domain_ep(self):
-        return self.V_ep.mesh
+        return self.ep_mesh
 
     def ep_values_to_function(self) -> None:
         for i in range(self.num_values):
@@ -83,19 +96,9 @@ class MissingValue:
     def interpolate_ep_to_mechanics(self) -> None:
         logger.debug("Interpolate ep to mechanics")
         for i in range(self.num_values):
-            logger.debug(f"Interpolate {i}")
-            self.u_mechanics[i].interpolate_nonmatching(
-                self.u_ep_int[i],
-                self.cells_ep2mech,
-                self.V_ep_interpolation_data_ep2mech,
-            )
+            self.transfer_ep2mech.interpolate(self.u_ep_int[i], self.u_mechanics[i])
 
     def interpolate_mechanics_to_ep(self) -> None:
         logger.debug("Interpolate mechanics to ep")
         for i in range(self.num_values):
-            logger.debug(f"Interpolate {i}")
-            self.u_ep[i].interpolate_nonmatching(
-                self.u_mechanics_int[i],
-                self.cells_mech2ep,
-                self.V_mechanics_interpolation_data_mech2ep,
-            )
+            self.transfer_mech2ep.interpolate(self.u_mechanics_int[i], self.u_ep[i])
